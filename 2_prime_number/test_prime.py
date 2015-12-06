37b15851f80a36ca9326c0908529467a4a9fc70f
midorikawa <midorikawa.keita@leadinge.co.jp>
2015-12-06 23:14:54 +0900

    prime現状最速

--- 2_prime_number/test_prime.py
+++ 2_prime_number/test_prime.py
@@ -5,8 +5,7 @@ from prime import Prime
 class MyTestCase(unittest.TestCase):
     def test_init(self):
         test_cls = Prime(20)
-        self.assertEqual(test_cls.input_num_list, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
-        self.assertEqual(test_cls.prime_list, [])
+        self.assertEqual(test_cls.num_list, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 
     def test_error(self):
         try:
@@ -22,13 +21,6 @@ class MyTestCase(unittest.TestCase):
         else:
             self.assertEqual(True, False)
 
-    def test_judge_num(self):
-        test_cls = Prime(20)
-        a = test_cls._judge_num(5, [2, 3])
-        b = test_cls._judge_num(4, [2, 3])
-        self.assertEqual(a, True)
-        self.assertEqual(b, False)
-
     def test_remove_prime_from_list(self):
         test_cls = Prime(20)
         list_a = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
@@ -43,6 +35,10 @@ class MyTestCase(unittest.TestCase):
                          [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
                           97])
 
+    def test_judge_all_2(self):
+        test_cls = Prime(2)
+        ret = test_cls.judge_all()
+        self.assertEqual(ret, [2])
 
 if __name__ == '__main__':
     unittest.main()
